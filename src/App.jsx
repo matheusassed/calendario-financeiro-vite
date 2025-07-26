4dffb53b8ed010b3861ab3273f94b29ea8e02290
Matheus Assed <matheusassed@gmail.com>
2025-07-26 14:35:43 -0300

    Revert "adiciona modal de ação para transações recorrentes; implementa lógica para edição e exclusão de transações recorrentes"
    This reverts commit e349659a73dd76d6927f61303022136d73da470b.

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
   const [calendarDate, setCalendarDate] = useState(new Date())
   const [viewMode, setViewMode] = useState('fiscal')
   const [selectedInvoiceId, setSelectedInvoiceId] = useState(null)
-  const [isRecurrenceModalOpen, setIsRecurrenceModalOpen] = useState(false)
 
   // --- Funções de Navegação de Dia ---
   const handleNextDay = () => {
@@ -123,8 +122,6 @@ function App() {
       setSelectedInvoiceId,
       viewMode,
       setViewMode,
-      isRecurrenceModalOpen,
-      setIsRecurrenceModalOpen,
     }
 
     if (loadingData && !loadingAuth) {
